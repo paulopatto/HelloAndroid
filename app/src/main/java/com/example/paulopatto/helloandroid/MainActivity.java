b0e52cb803230baf29ad1efad458bdb9658b26fe
Paulo Patto <paulopatto@paulopatto.com>
2016-07-05 00:35:00 -0300

    [feature/add_action_to_button]: Link UI components with codebihind

--- app/src/main/java/com/example/paulopatto/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/paulopatto/helloandroid/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.paulopatto.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private TextView saudacaoTextView;
+    private String saudacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /// Obter referencias aos componentes da view
+
+        this.nameEditText = findViewById(R.id.nameEditText);
+        this.saudacaoTextView = findViewById(R.id.saudacaoTextView);
+        this.saudacao = getResources().getString(R.string.saudacao);
+    }
+
+    public void surpreenderUsaurio(View v) {
+        Editable texto = this.nameEditText.getText();
+        this.saudacaoTextView.setText(texto);
     }
 }
